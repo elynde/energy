1189a772eda390c50cb9cd9c65b3d47bda51f254
Elliot Lynde <elliotlynde@gmail.com>
2012-11-04 00:08:43 -0700

    use next hour for starting alarm instead of last otherwise it fires on every boot

--- src/com/example/StartupReceiver.java
+++ src/com/example/StartupReceiver.java
@@ -13,8 +13,8 @@ public class StartupReceiver extends BroadcastReceiver {
         AlarmManager alarmManager = (AlarmManager)c.getSystemService(c.ALARM_SERVICE);
 
         long time = System.currentTimeMillis()/1000;
-        long last_hour = time - (time % 3600);
+        long next_hour = time - (time % 3600) + 3600;
         PendingIntent sender = PendingIntent.getBroadcast(c, 0, new Intent(c, Alarm.class), 0);
 
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, last_hour * 1000, 60 * 60 * 1000, sender);    }
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, next_hour * 1000, 60 * 60 * 1000, sender);    }
 }
